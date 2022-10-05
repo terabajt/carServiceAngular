79e4abfe17e3964915ac0679911a76c13374ef36
Michał Pasynek <michalpasynek@gmail.com>
2022-10-05 20:18:03 +0200

    Init commit

---
+++ src/app/cars/cars-list/cars-list.component.ts
@@ -0,0 +1,66 @@
+import { ViewEncapsulation } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Car } from '../models/car';
+
+@Component({
+  selector: 'cars-list',
+  templateUrl: './cars-list.component.html',
+  styleUrls: ['./cars-list.component.less'],
+  encapsulation: ViewEncapsulation.None,
+})
+export class CarsListComponent implements OnInit {
+  totalCost: number | undefined;
+  cars: Car[] = [
+    {
+      id: 1,
+      model: 'Mazda Rx7',
+      plate: 'GD2121E',
+      deliveryDate: '21-04-2017',
+      deadline: '05-05-2016',
+      client: {
+        firstName: 'Jan',
+        surname: 'Kowalski',
+      },
+      cost: 300,
+      isFullyDamaged: true,
+    },
+    {
+      id: 2,
+      model: 'Mercedes 124',
+      plate: 'KRK2200',
+      deliveryDate: '24-05-2017',
+      deadline: '03-06-2016',
+      client: {
+        firstName: 'Michał',
+        surname: 'Nowak',
+      },
+      cost: 1200,
+      isFullyDamaged: false,
+    },
+    {
+      id: 3,
+      model: 'Renault CLIO',
+      plate: 'GWE22011',
+      deliveryDate: '02-02-2017',
+      deadline: '03-03-2017',
+      client: {
+        firstName: 'Beata',
+        surname: 'Dampc',
+      },
+      cost: 2800,
+      isFullyDamaged: true,
+    },
+  ];
+
+  constructor() {}
+
+  ngOnInit(): void {
+    this.countTotalCost();
+  }
+
+  countTotalCost(): void {
+    this.totalCost = this.cars
+      .map((car) => car.cost) // 300, 400, 600
+      .reduce((prev, next) => prev + next);
+  }
+}
